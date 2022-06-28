c5d19e50e7a48f83083cf9dd5e5a39708e6db5cb
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-06-28 11:08:24 -0300

    [feat] function to search pokemon

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,21 @@
 import * as S from './style'
+import { useState } from 'react'
+import { getPokemonData } from '../../api'
 
 export const Form = () => {
 
+  const [inputValue, setInputValue] = useState("")
+  const [pokemonn, setPokemonn] = useState()
+
   const handleSubmit = event => {
     event.preventDefault()
+    onSearchHandle(inputValue)
+  }
+
+  const onSearchHandle = async pokemon => {
+    const result = await getPokemonData(pokemon)
+    setPokemonn(result)
+    console.log("pokemon: ", pokemonn)
   }
 
   return (
@@ -12,9 +24,18 @@ export const Form = () => {
         <input type="text"
           autoFocus="on"
           placeholder='Procure um Pokemon'
+          id="teste"
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <button>Enviar</button>
       </form>
+      {pokemonn ? (
+        <div>
+          <div>Nome: {pokemonn.name}</div>
+          <div>Order: {pokemonn.moves[0].move.url}</div>
+          <img src={pokemonn.sprites.front_default} alt="" />
+        </div>
+      ) : null}
     </S.Bg>
   )
 }
