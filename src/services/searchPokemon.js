c802932867fa7a7b5930d5148c0f1979174ab629
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-04 21:24:46 -0300

    [chore] file name change

---
+++ src/services/searchPokemon.js
@@ -0,0 +1,6 @@
+import { searchPokemonData } from "./api"
+
+export const onSearchHandle = async (pokemon, setPokemon) => {
+  const result = await searchPokemonData(pokemon)
+  setPokemon(result)
+}
