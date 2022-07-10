c0f5528a33f80110f5155ae928cbeada5f5ba5b3
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-10 18:39:17 -0300

    test adjustments

---
+++ src/components/Footer/index.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react"
+import renderer from 'react-test-renderer'
+
+import { Footer } from "."
+
+describe("FooterComponent", () => {
+  it ("component rendering footer", () => {
+    const tree = renderer
+    .create(<Footer />)
+    expect(tree).toBeTruthy()
+  })
+
+  it ("must contain this paragraph Desenvolvido por Felipe Cardoso", () => {
+    render(<Footer />)
+    const footerParagraph = screen.getByText("Desenvolvido por Felipe Cardoso")
+    expect(footerParagraph).toBeTruthy()
+  })
+
+  it ("component rendering", () => {
+    const tree = renderer
+    .create(<Footer />)
+    .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
