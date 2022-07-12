1b58d814fa58f7773727a447072ccfe304a720ed
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-12 14:45:27 -0300

    add link to home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import * as S from "./style"
+import { Link } from 'react-router-dom'
 import pokedex from "../../assets/pokedex.png"
 import pokebola from "../../assets/favicon.png"
 
@@ -7,7 +8,7 @@ export const Header = () => {
   return (
     <S.Bg>
       <S.Main>
-        <img className="logo" src={pokedex} alt="logo pokedex" />
+        <Link to='/pokedex/' ><img className="logo" src={pokedex} alt="logo pokedex" /></Link>
         <img className="pokebola" src={pokebola} alt="pokebola" />
       </S.Main>
     </S.Bg>
