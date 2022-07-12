484dcadf59e2be0138302080748c4e97434e00d5
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-12 14:44:44 -0300

    created page for search pokemon

---
+++ src/pages/SearchPokemon/index.jsx
@@ -0,0 +1,13 @@
+import * as S from './style'
+import { Footer, Form, Header } from '../../components'
+
+export const SearchPokemon = () => {
+
+  return (
+    <div>
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  )
+}
