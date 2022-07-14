fb3c314d2eadd8a3b61a6baaeffcc1c90bae584d
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-14 19:17:54 -0300

    [refactor] destructuring on pokemon props to avoid repetition

--- src/components/Pokemon/index.jsx
+++ src/components/Pokemon/index.jsx
@@ -4,14 +4,15 @@ import * as S from './style'
 export const Pokemon = props => {
 
   const { pokemon, large, medium } = props
+  const { id, name, sprites } = pokemon
 
   const pokemonType = pokemon.types[0].type.name
   return (
     <S.Pokemon pokemonType={pokemonType} large={large} medium={medium}>
       <div className='pokemonName'>{firstLetterUpperCase(pokemon)}</div>
-      <div className='pokemonId'>#{pokemon.id}</div>
+      <div className='pokemonId'>#{id}</div>
       <div className='img'>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <img src={sprites.front_default} alt={name} />
       </div>
       <div className='pokemonType'>{firstLetterUpperCaseType(pokemon)}</div>
     </S.Pokemon>
