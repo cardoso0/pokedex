f6c40a18c5af95cc88090e14657aab90da1ebcf5
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-14 00:18:19 -0300

    [refactor] starting refactor using axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://pokeapi.co/api/v2/",
+});
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+      return Promise.reject(error)
+  }
+)
+
+export default api;
+
 const fetchData = async url => {
   try {
     const response = await fetch(url)
