718e25660fad58e17f59590af65aa5e7a9907557
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-19 13:19:19 -0300

    test(Pokemon): created tests for the component

--- src/components/Pokemon/index.test.js
+++ src/components/Pokemon/index.test.js
@@ -1,13 +1,55 @@
-// import { render } from "@testing-library/react"
-// import renderer from 'react-test-renderer'
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import renderer from "react-test-renderer"
 
-// import { Pokemon } from "."
+import { Pokemon } from "."
 
-// describe("Pokemon Component", () => {
-//   it ("component rendering | snapshot", () => {
-//     const tree = renderer
-//     .create(<Pokemon />)
-//     .toJSON()
-//     expect(tree).toMatchSnapshot()
-//   })
-// })
+const pokemon = {
+  name: "Pikachu",
+  sprites: "front_default",
+  id: 1,
+  types: [{type: {name: "electric"}}],
+}
+
+const renderPokemon = 
+<Pokemon pokemon={pokemon} large="string" medium="string"/>
+
+describe("Pokemon Component", () => {
+  it ("component rendering | snapshot", () => {
+    const tree = renderer
+    .create(renderPokemon)
+    .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it ("component rendering", () => {
+    expect(render(renderPokemon)).toBeTruthy
+  })
+})
+
+describe("Rendering pokemon data", () => {
+  it ("render pokemon name", () => {
+    render(renderPokemon)
+    const name = screen.getByText("Pikachu")
+    expect(name).toBeTruthy()
+  })
+
+  it ("render pokemon id", () => {
+    render(renderPokemon)
+    const id = screen.getByText("#1")
+    expect(id).toBeTruthy()
+  })
+
+  it("checking img alt", () => {
+    render(renderPokemon)
+    const img = screen.getByRole("img")
+    expect(img).toHaveAttribute("alt", "Pikachu")
+  })
+
+  it("render pokemon type", () => {
+    render(renderPokemon)
+    const type = screen.getByText("Electric")
+    expect(type).toBeTruthy()
+  })
+
+})
