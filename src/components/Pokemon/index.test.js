6acf7326eda61695eb92e788a224884e1b8ad120
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-19 13:16:44 -0300

    test(Pokemon): add snapshot

---
+++ src/components/Pokemon/index.test.js
@@ -0,0 +1,13 @@
+// import { render } from "@testing-library/react"
+// import renderer from 'react-test-renderer'
+
+// import { Pokemon } from "."
+
+// describe("Pokemon Component", () => {
+//   it ("component rendering | snapshot", () => {
+//     const tree = renderer
+//     .create(<Pokemon />)
+//     .toJSON()
+//     expect(tree).toMatchSnapshot()
+//   })
+// })
