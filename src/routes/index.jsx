a497e9277b936f1b8d76bd4f68d877c231564b67
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-21 15:12:26 -0300

    chore(PD): remove context provider

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import { ContextProvider } from '../contexts/Context'
 import { Home, SearchPokemon } from '../pages'
 
 export const MainRoutes = () => {
   return (
-    <ContextProvider>
-      <Routes>
-        <Route path='/pokedex/' element={<Home />} />
-        <Route path='/pokedex/searchpokemon' element={<SearchPokemon />} />
-      </Routes>
-    </ContextProvider>
+    <Routes>
+      <Route path='/pokedex/' element={<Home />} />
+      <Route path='/pokedex/searchpokemon' element={<SearchPokemon />} />
+    </Routes>
   )
 }
