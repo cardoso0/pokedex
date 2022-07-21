9475cba489e665bb59c2bed08fab80e8943a9a54
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-21 16:14:11 -0300

    refactor(PD): this page is now responsible for rendering pokemons and error

--- src/pages/SearchPokemon/index.jsx
+++ src/pages/SearchPokemon/index.jsx
@@ -1,13 +1,26 @@
-import { Footer, Form, Header } from '../../components'
-import { ContextProvider } from '../../contexts/Context'
+import { Error } from '../../components/Form/style'
+import { useContext } from 'react'
+import { Footer, Form, Header, Pokemon } from '../../components'
+import { Context } from '../../contexts/Context'
+import whosThatPokemon from '../../assets/whosThatPokemon.png'
 
 export const SearchPokemon = () => {
 
+  const { pokemon, isSearched } = useContext(Context)
+
   return (
-    <ContextProvider>
+    <div>
       <Header />
+      {isSearched &&
+        <div>
+          {pokemon
+            ? <Pokemon pokemon={pokemon} large={'20%'} medium={'25%'} />
+            : <Error><img src={whosThatPokemon} alt='Quem é esse Pokemon?' /></Error>
+          }
+        </div>
+      }
       <Form />
       <Footer />
-    </ContextProvider>
+    </div>
   )
 }
