fd37a50ef3f1ae115e380ff4b69eac7e7495d9d4
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-23 18:39:54 -0300

    feat(PD): created state for pokemon saved

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -5,9 +5,10 @@ export const Context = createContext()
 export const ContextProvider = (props) => {
   const [pokemon, setPokemon] = useState()
   const [isSearched, setIsSearched] = useState(false)
+  const [pokemonSaved, setPokemonSaved] = useState(null)
   
   return (
-    <Context.Provider value={{ pokemon, setPokemon, isSearched, setIsSearched }}>
+    <Context.Provider value={{ pokemon, setPokemon, isSearched, setIsSearched, pokemonSaved, setPokemonSaved }}>
       {props.children}
     </Context.Provider>
   )
