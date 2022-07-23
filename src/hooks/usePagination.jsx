59575cdef1e2dbfdc7c3abad60e47c4e78682fe9
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-23 18:32:47 -0300

    refactor(PD): verify pokemon saved to set pagination

--- src/hooks/usePagination.jsx
+++ src/hooks/usePagination.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { Context } from "../contexts/Context"
 
 export const usePagination = props => {
   const { pokemons } = props
 
+  const { pokemonSaved } = useContext(Context)
   const [pageNumber, setPageNumber] = useState(0)
+
+  const verify = 
+    pokemonSaved
+      ? pokemonSaved
+      : pokemons
 
   const itensPerPage = 24
   const pagesVisited = pageNumber * itensPerPage
-  const pageCount = Math.ceil(pokemons.length / itensPerPage)
+  const pageCount = Math.ceil(verify.length / itensPerPage)
   const changePage = ({ selected }) => {
     setPageNumber(selected)
   }
