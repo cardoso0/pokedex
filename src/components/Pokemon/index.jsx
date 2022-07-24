2110024b0d4c5c7b108f39e94b1b2a76884e5e7a
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-23 23:00:08 -0300

    chore(PD): add heart

--- src/components/Pokemon/index.jsx
+++ src/components/Pokemon/index.jsx
@@ -6,15 +6,18 @@ export const Pokemon = props => {
   const { pokemon, large, medium, handlePokemon } = props
   const { id, name } = pokemon
 
-  const sprite = 
+  const sprite =
     `https://cdn.traction.one/pokedex/pokemon/${id}.png`
-    // `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+  // `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
 
   const pokemonType = pokemon.types[0].type.name
   return (
     <S.Pokemon pokemonType={pokemonType} large={large} medium={medium} onClick={handlePokemon} >
-      <div className='pokemonName'>{firstLetterUpperCase(pokemon)}</div>
-      <div className='pokemonId'>#{id}</div>
+      <S.BgName>
+        <div className='pokemonId'>#{id}</div>
+        <div className='pokemonName'>{firstLetterUpperCase(pokemon)}</div>
+        <div className='heart'>🤍</div>
+      </S.BgName>
       <div className='img'>
         <img src={sprite} alt={name} />
       </div>
