34644448db7acaa9ffb3f98e042ae37378e0e083
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-25 22:26:31 -0300

    feat(PD): added favorites state

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -6,9 +6,10 @@ export const ContextProvider = (props) => {
   const [pokemon, setPokemon] = useState()
   const [isSearched, setIsSearched] = useState(false)
   const [pokemonSaved, setPokemonSaved] = useState(null)
-  
+  const [favorites, setFavorites] = useState([])
+
   return (
-    <Context.Provider value={{ pokemon, setPokemon, isSearched, setIsSearched, pokemonSaved, setPokemonSaved }}>
+    <Context.Provider value={{ pokemon, setPokemon, isSearched, setIsSearched, pokemonSaved, setPokemonSaved, favorites, setFavorites }}>
       {props.children}
     </Context.Provider>
   )
