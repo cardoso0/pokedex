cbf835547cd69547df8aba5ba93cf7b7c662a88f
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-25 23:42:17 -0300

    style(PD): added grid

--- src/pages/Favorites/style.js
+++ src/pages/Favorites/style.js
@@ -2,4 +2,20 @@ import styled from 'styled-components'
 
 export const Height = styled.div`
   min-height: 100vh;
+  max-width: 90%;
+  margin: 0 auto;
+`
+export const Pokemons = styled.div`
+  display: grid;
+  margin-top: 2rem;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    width: 95%;
+    margin: 0 auto;
+  }
 `
