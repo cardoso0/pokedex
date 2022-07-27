b96368f0e76c01e3ee41eb7cd38a666f7e67c365
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-27 00:05:26 -0300

    chore(PD): update imports/exports

--- src/shared/index.js
+++ src/shared/index.js
@@ -2,10 +2,14 @@ import { colors } from "./colorsBgPokemons";
 import { firstLetterUpperCase } from "./firstLetterUpperCase";
 import { handleFavorite } from "./handleFavorite"
 import { verifyFavorite } from "./verifyFavorite"
+import { loadFavoritePokemons } from "./loadFavoritePokemons"
+import { navigateToSearchPokemon } from "./navigateToSearchPokemon"
 
 export {
   colors,
   firstLetterUpperCase,
   handleFavorite,
-  verifyFavorite
+  verifyFavorite,
+  loadFavoritePokemons,
+  navigateToSearchPokemon
 }
