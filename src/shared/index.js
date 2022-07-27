0315b68634f053d1ee923f6837d429065fc73f16
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-26 23:01:50 -0300

    chore(PD): created index for imports and exports

---
+++ src/shared/index.js
@@ -0,0 +1,11 @@
+import { colors } from "./colorsBgPokemons";
+import { firstLetterUpperCase } from "./firstLetterUpperCase";
+import { handleFavorite } from "./handleFavorite"
+import { verifyFavorite } from "./verifyFavorite"
+
+export {
+  colors,
+  firstLetterUpperCase,
+  handleFavorite,
+  verifyFavorite
+}
