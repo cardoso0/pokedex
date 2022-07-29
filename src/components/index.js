4fd8e5a68f76b7d5880c7b450874e8ba8e088144
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-29 16:08:31 -0300

    chore(PD): added menu

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import { Footer } from './Footer'
 import { Form } from './Form'
 import { Header } from './Header'
 import { Pokemon } from './Pokemon'
+import { Menu } from './Menu'
 
 export {
   Footer,
   Form,
   Header,
-  Pokemon
+  Pokemon,
+  Menu
 }
