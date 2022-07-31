216f32b7e387200063bb65bbfe96ebcb1259f5f1
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-07-31 18:43:37 -0300

    feat(PD): added more details about pokemons

--- src/pages/DetailsPokemon/index.jsx
+++ src/pages/DetailsPokemon/index.jsx
@@ -9,7 +9,14 @@ import { handleFavorite, verifyFavorite } from '../../shared'
 export const DetailsPokemon = () => {
 
   const { pokemon, isSearched, favorites, setFavorites } = useContext(Context)
-  console.log(pokemon)
+  let move = []
+  pokemon.moves
+    .map(item => move.push(item.move.name))
+
+  const firstLetterUpperCase = (n) =>
+    move[n][0].toUpperCase() + move[n].substring(1)
+  const pokemonType = pokemon.types[0].type.name
+
   return (
     <div>
       <Header />
@@ -18,20 +25,24 @@ export const DetailsPokemon = () => {
         {isSearched &&
           <div>
             {pokemon
-              ? <>
-              <Pokemon
-                pokemon={pokemon}
-                large={'20%'}
-                medium={'25%'}
-                handleFavorite={() => handleFavorite(pokemon, favorites ,setFavorites)}
-                heart={verifyFavorite(pokemon, favorites)}
-                
-              />
-              <div>XP: {pokemon.base_experience}</div>
-              <div>Habilidade: {pokemon.abilities[0].ability.name}</div>
-              <div>Altura: {pokemon.height} m</div>
-              <div>Peso: {pokemon.weight} Kg</div>
-              </>
+              ? <S.Card>
+                <S.Details pokemonType={pokemonType}>
+                  <li><span>HP:</span> {pokemon.stats[0]['base_stat']}</li>
+                  <li><span>Ataque:</span> {pokemon.stats[1]['base_stat']}</li>
+                  <li><span>Velocidade:</span> {pokemon.stats[5]['base_stat']}</li>
+                  <li><span>Atq¹:</span> {firstLetterUpperCase(0)}</li>
+                  <li><span>Atq²:</span> {firstLetterUpperCase(1)}</li>
+                  <li><span>Atq³:</span> {firstLetterUpperCase(2)}</li>
+                  <li><span>Atq⁴:</span> {firstLetterUpperCase(3)}</li>
+                </S.Details>
+                <Pokemon pkBg={'pokemonBg'}
+                  pokemon={pokemon}
+                  large={'20%'}
+                  medium={'25%'}
+                  handleFavorite={() => handleFavorite(pokemon, favorites, setFavorites)}
+                  heart={verifyFavorite(pokemon, favorites)}
+                />
+              </S.Card>
               : <Error><img src={whosThatPokemon} alt='Quem é esse Pokemon?' /></Error>
             }
           </div>
