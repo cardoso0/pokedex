e441e3cce27920f28cc92575a31ce408ce42db88
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-01 22:27:54 -0300

    config(PD): import i18n config

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import "./i18n/index"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
