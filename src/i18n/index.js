47114ce10093cf9d9151ae246a8ba02cadca6d08
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-02 08:49:21 -0300

    config(PD): i18n configured

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,13 +1,19 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
+import enJson from './translations/en.json'
+import ptBRJson from './translations/ptBR.json'
+
 i18n.use(initReactI18next).init({
+  lng: navigator.language,
+  // fallbackLng: "ptBR",
   interpolation: {
     escapeValue: false,
   },
   resources: {
-
-  }
+    'en-US': enJson,
+    'pt-BR': ptBRJson,
+  },
 })
 
 export default i18n
