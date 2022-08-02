12965361890f29e77cdc3fe306e16363bf28bb6d
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-02 15:10:33 -0300

    chore(PD): adjustments in config

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,17 +1,17 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 
-import enJson from './translations/en.json'
+import enUSJson from './translations/enUS.json'
 import ptBRJson from './translations/ptBR.json'
 
 i18n.use(initReactI18next).init({
   lng: navigator.language,
-  // fallbackLng: "ptBR",
+  // fallbackLng: "en-US",
   interpolation: {
     escapeValue: false,
   },
   resources: {
-    'en-US': enJson,
+    'en-US': enUSJson,
     'pt-BR': ptBRJson,
   },
 })
