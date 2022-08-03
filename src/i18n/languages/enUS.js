59b06abd7715477c86974d62efa55ee32285f4d8
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-02 21:11:44 -0300

    refactor(PD): changed translations structure from json to js

---
+++ src/i18n/languages/enUS.js
@@ -0,0 +1,21 @@
+const messages = {
+  'en-US': {
+    translations: {
+      Title: {
+        title: "Are you ready for this journey?",
+        subtitle: "Form your Pokemon team!",
+      },
+      Menu: {
+        fav: "favorites",
+      },
+      Form: {
+        placeholder: "Type a Pokemon",
+      },
+      Footer: {
+        copy: "Developed by Felipe Cardoso"
+      }
+    }
+  }
+}
+
+export { messages }
---
+++ src/i18n/languages/ptBR.js
@@ -0,0 +1,21 @@
+const messages = {
+  'pt-BR': {
+    translations: {
+      Title: {
+        title: "Está preparado para essa jornada?",
+        subtitle: "Forme sua equipe pokémon!",
+      },
+      Menu: {
+        fav: "favoritos",
+      },
+      Form: {
+        placeholder: "Digite um Pokemon",
+      },
+      Footer: {
+        copy: "Desenvolvido por Felipe Cardoso"
+      }
+    }
+  }
+}
+
+export { messages }
