98853f67a6d3c35fee843ec82eb5cead061d6602
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-05 22:42:55 -0300

    chore(PD): added trainers path

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { ContextProvider } from '../contexts/Context'
-import { Favorites, Pokedex, DetailsPokemon } from '../pages'
+import { Favorites, Pokedex, DetailsPokemon, Trainers } from '../pages'
 
 export const MainRoutes = () => {
   return (
     <ContextProvider>
       <Routes>
         <Route path='/pokedex/' element={<Pokedex />} />
+        <Route path='/pokedex/trainers' element={<Trainers />} />
         <Route path='/pokedex/detailspokemon' element={<DetailsPokemon />} />
         <Route path='/pokedex/favorites' element={<Favorites />} />
       </Routes>
