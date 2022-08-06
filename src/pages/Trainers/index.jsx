d480672be1554316e8ab22577c09fa79c6d81b3a
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-05 23:09:39 -0300

    feat(PD): page sketch

---
+++ src/pages/Trainers/index.jsx
@@ -0,0 +1,19 @@
+
+import { Footer, Header, Title } from "../../components"
+
+
+export const Trainers = () => {
+
+  return (
+    <>
+      <Header />
+        <div>
+          <Title
+            title={'Está pronto para iniciar a jornada?'}
+            subtitle={'Escolha um Treinador'}
+          />
+        </div>
+      <Footer />
+    </>
+  )
+}
