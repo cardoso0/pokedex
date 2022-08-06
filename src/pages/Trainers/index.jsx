79b4b2dd369fbee84bb869ab20a1faa780f4a31d
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-06 00:29:17 -0300

    feat(PD): added trainer

--- src/pages/Trainers/index.jsx
+++ src/pages/Trainers/index.jsx
@@ -1,6 +1,5 @@
-
-import { Footer, Header, Title } from "../../components"
-
+import { Footer, Header, Title, Trainer } from "../../components"
+import ash from "../../assets/trainers/ash.png"
 
 export const Trainers = () => {
 
@@ -11,6 +10,18 @@ export const Trainers = () => {
           <Title
             title={'Está pronto para iniciar a jornada?'}
             subtitle={'Escolha um Treinador'}
+          />
+          <Trainer 
+            name={'Ash'}
+            image={ash}
+            age={'Idade'}
+            textAge={'10 anos'}
+            region={'Região'}
+            textRegion={'Kanto'}
+            city={'Cidade'}
+            textCity={'Pallet'}
+            obsA={'Treinador novato'}
+            obsB={'Almeja vencer a liga Pokemon'}
           />
         </div>
       <Footer />
