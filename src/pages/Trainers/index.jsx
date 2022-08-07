cf48e2d8a5752fdad76505f58156cfee2437904d
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-06 22:10:43 -0300

    feat(PD): added all trainers

--- src/pages/Trainers/index.jsx
+++ src/pages/Trainers/index.jsx
@@ -1,29 +1,34 @@
+import * as S from './style'
 import { Footer, Header, Title, Trainer } from "../../components"
-import ash from "../../assets/trainers/ash.png"
+import { trainers } from "../../components/Trainer/trainers"
 
 export const Trainers = () => {
-
+  // console.log(trainers[0].name)
   return (
     <>
       <Header />
-        <div>
-          <Title
-            title={'Está pronto para iniciar a jornada?'}
-            subtitle={'Escolha um Treinador'}
-          />
-          <Trainer 
-            name={'Ash'}
-            image={ash}
-            age={'Idade'}
-            textAge={'10 anos'}
-            region={'Região'}
-            textRegion={'Kanto'}
-            city={'Cidade'}
-            textCity={'Pallet'}
-            obsA={'Treinador novato'}
-            obsB={'Almeja vencer a liga Pokemon'}
-          />
-        </div>
+      <S.Bg>
+        <Title
+          title={'Está pronto para iniciar a jornada?'}
+          subtitle={'Escolha um Treinador'}
+        />
+        <S.Trainers>
+          {trainers.map(item =>
+            <Trainer
+              name={item.name}
+              image={item.image}
+              age={'Idade'}
+              textAge={item.textAge}
+              region={'Região'}
+              textRegion={item.textRegion}
+              city={'Cidade'}
+              textCity={item.textCity}
+              obsA={item.obsA}
+              obsB={item.obsB}
+            />
+          )}
+        </S.Trainers>
+      </S.Bg>
       <Footer />
     </>
   )
