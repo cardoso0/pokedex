3ba1553b5a5b4f13860725c8fdb8fe0bb207723e
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-06 22:19:23 -0300

    style(PD): style responsive

---
+++ src/pages/Trainers/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Bg = styled.div`
+  width: 95%;
+  margin: 0 auto;
+`
+
+export const Trainers = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
