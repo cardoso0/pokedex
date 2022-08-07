d5a12fd5d6c3a2b4082af46c23b0c6428af2b60c
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-07 00:04:59 -0300

    feat(PD): added trainer in localstorage

--- src/pages/Trainers/index.jsx
+++ src/pages/Trainers/index.jsx
@@ -1,8 +1,15 @@
 import * as S from './style'
 import { Footer, Header, Title, Trainer } from "../../components"
 import { trainers } from "../../components/Trainer/trainers"
+import { useState } from 'react'
 
 export const Trainers = () => {
+
+  const [trainer, setTrainer] = useState('')
+
+  const handleClick = (name) => {
+    setTrainer(name)
+  }
 
   return (
     <>
@@ -15,6 +22,7 @@ export const Trainers = () => {
         <S.Trainers>
           {trainers.map(item =>
             <Trainer
+              select={() => handleClick(item.name)}
               name={item.name}
               image={item.image}
               age={'Idade'}
@@ -30,7 +38,7 @@ export const Trainers = () => {
         </S.Trainers>
         <S.CallToAction>
           <h1>Não vacile igual ao Ash</h1>
-          <button>Forme a sua equipe Pokémon!</button>
+          <button onClick={() => localStorage.setItem('trainer', trainer)}>Forme a sua equipe Pokémon!</button>
         </S.CallToAction>
       </S.Bg>
       <Footer />
