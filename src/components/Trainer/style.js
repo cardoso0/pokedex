4ace9714d1e5c7941bffae7300820bb04aad4835
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-08 12:09:39 -0300

    chore(PD): changed color to hexa

--- src/components/Trainer/style.js
+++ src/components/Trainer/style.js
@@ -10,7 +10,7 @@ export const Card = styled.div`
   }
   figure {
     text-align: center;
-    border: 1px solid black;
+    border: 1px solid #000;
     border-radius: 10px 10px 0 0;
     cursor: pointer;
     img {
@@ -22,20 +22,20 @@ export const Card = styled.div`
 
 export const Info = styled.div`
   margin-top: 0.5rem;
-  border: 1px solid black;
+  border: 1px solid #000;
   border-radius: 0 0 10px 10px;
   height: 200px;
 
   th {
     background: #FF6464;
-    border-right: 1px solid black;
-    border-bottom: 1px solid black;
+    border-right: 1px solid #000;
+    border-bottom: 1px solid #000;
     padding: 10px 20px 10px 10px;
   }
   td {
     text-align: center;
     width: 100%;
-    border-bottom: 1px solid black;
+    border-bottom: 1px solid #000;
   }
 
   table + p {
