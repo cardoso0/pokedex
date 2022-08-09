6f092e93c0d8150ac85df25d3b632781b7d7ccfc
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-09 14:31:16 -0300

    chore(PD): added context trainer provider

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import { ContextProvider } from '../contexts/Context'
+import { ContextTrainerProvider } from '../contexts/ContextTrainer'
 import { Favorites, Pokedex, DetailsPokemon, Trainers } from '../pages'
 
 export const MainRoutes = () => {
   return (
     <ContextProvider>
-      <Routes>
-        <Route path='/pokedex/' element={<Trainers />} />
-        <Route path='/pokedex/pokedexx' element={<Pokedex />} />
-        <Route path='/pokedex/detailspokemon' element={<DetailsPokemon />} />
-        <Route path='/pokedex/favorites' element={<Favorites />} />
-      </Routes>
+      <ContextTrainerProvider>
+        <Routes>
+          <Route path='/pokedex/' element={<Trainers />} />
+          <Route path='/pokedex/pokedexx' element={<Pokedex />} />
+          <Route path='/pokedex/detailspokemon' element={<DetailsPokemon />} />
+          <Route path='/pokedex/favorites' element={<Favorites />} />
+        </Routes>
+      </ContextTrainerProvider>
     </ContextProvider>
   )
 }
