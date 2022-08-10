0d00b7e4edb12867233a0a1ddd1f001ef0289884
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-10 00:27:01 -0300

    feat(PD): added trainer saved in favorites

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,15 +1,17 @@
 import * as S from './style'
 import { useContext, useEffect } from 'react'
-import { Footer, Header, Pokemon } from '../../components'
+import { Footer, Header, Pokemon, Trainer } from '../../components'
 import { Context } from '../../contexts/Context'
 import { verifyFavorite, loadFavoritePokemons, navigateToDetailsPokemon } from '../../shared'
 import { useNavigate } from 'react-router-dom'
+import { ContextTrainer } from '../../contexts/ContextTrainer'
 
 export const Favorites = () => {
 
   const navigate = useNavigate()
 
   const { favorites, setFavorites, setPokemon, setIsSearched } = useContext(Context)
+  const { trainer } = useContext(ContextTrainer)
 
   const removeFavorite = (pokemon) => {
     let updatedFavorites = [...favorites]
@@ -26,16 +28,28 @@ export const Favorites = () => {
 
   useEffect(() => {
     loadFavoritePokemons(setFavorites)
-  },[])
+  }, [])
 
   return (
     <div>
       <Header />
       <S.Height>
+        <Trainer
+          name={trainer.name}
+          image={trainer.image}
+          age={'Idade'}
+          textAge={trainer.textAge}
+          region={'Região'}
+          textRegion={trainer.textRegion}
+          city={'Cidade'}
+          textCity={trainer.textCity}
+          obsA={trainer.obsA}
+          obsB={trainer.obsB}
+        />
         <S.Pokemons>
           {favorites.map((pokemon) => {
             return (
-              <Pokemon
+              <Pokemon pkBg={'pokemonBg'}
                 pokemon={pokemon}
                 key={pokemon.id}
                 handlePokemon={() => navigateToDetailsPokemon(pokemon, setPokemon, setIsSearched, navigate)}
