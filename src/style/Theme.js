2bbaac686b9fcf9b823ed298c03f19933c6bd67f
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-11 01:32:25 -0300

    style(PD): created styles for themes

---
+++ src/style/Theme.js
@@ -0,0 +1,14 @@
+export const contraste = {
+  color: {
+    background: '#191919',
+    text: '#fff',
+    border: '#fff'
+  }
+}
+
+export const normal = {
+  color: {
+    background: '#fff',
+    text: '#191919'
+  }
+}
---
+++ src/style/global.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    // box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  
+  body {
+    background: ${(props) => props.theme.color.background};
+    color: ${(props) => props.theme.color.text}};
+  }
+`
