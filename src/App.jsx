aafd76441c50999d86e35b90b8ea365fb8616f0d
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-15 09:01:58 -0300

    chore(PD): added theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { useTheme } from './hooks/useTheme'
 import { MainRoutes } from './routes'
+import { GlobalStyle } from './style/global'
 
 const App = () => {
 
+  const { verifyTheme } = useTheme()
+
   return (
     <div>
-      <MainRoutes />
+      <ThemeProvider theme={verifyTheme}>
+        <GlobalStyle />
+        <MainRoutes />
+      </ThemeProvider>
     </div>
   )
 }
--- src/components/MenuAccessibility/index.jsx
+++ src/components/MenuAccessibility/index.jsx
@@ -1,30 +1,16 @@
-import contrast from '../../style/themes/contrast'
-import normal from '../../style/themes/normal'
 import { GlobalStyle } from "../../style/global"
 import { ThemeProvider } from "styled-components"
-import { useState } from "react"
+import { useTheme } from '../../hooks/useTheme'
 
 export const MenuAccessibility = () => {
 
-
-  const [tema, setTema] = useState(normal)
-
-  const theme = JSON.parse(localStorage.getItem('hightContrast'))
-  const switchTheme = () => {
-    if (theme.title === "contrast") {
-      localStorage.setItem('hightContrast', JSON.stringify(normal))
-      setTema(theme)
-    } else {
-      localStorage.setItem('hightContrast', JSON.stringify(contrast))
-      setTema(theme)
-    }
-  }
+  const { switchTheme, verifyTheme } = useTheme()
 
   return (
-    <ThemeProvider theme={tema}>
+    <ThemeProvider theme={verifyTheme}>
       <GlobalStyle />
       <div>
-        <button onClick={() => switchTheme()}>Tema atual: {tema.title}</button>
+        <button onClick={() => switchTheme()}>Tema atual: </button>
       </div>
     </ThemeProvider>
   )
