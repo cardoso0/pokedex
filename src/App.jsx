f6d9202aad94886e09c43813daea3d48d79e409e
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-15 17:47:56 -0300

    chore(PD): context call adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { MainRoutes } from './routes'
 import { GlobalStyle } from './style/global'
 import { CustomThemeProvider } from './contexts/CustomTheme'
-import { useTheme } from './hooks/useTheme'
-import { Header } from './components'
-const App = () => {
 
-  const { switchTheme } = useTheme()
+const App = () => {
 
   return (
     <CustomThemeProvider>
       <GlobalStyle />
-      <Header switch={switchTheme}/>
       <MainRoutes />
     </CustomThemeProvider>
   )
