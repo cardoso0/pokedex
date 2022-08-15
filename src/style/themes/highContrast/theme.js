4fbdfcafd1bb2483f3e9c91e7d4f81100bec93b1
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-15 14:26:52 -0300

    chore(PD): added new folder to themes

---
+++ src/style/themes/highContrast/theme.js
@@ -0,0 +1,21 @@
+export const normal = {
+  title: 'normal',
+
+  colors: {
+    primary: '#ff0400',
+    background: '#fff',
+    text: '#191919',
+    border: 'none'
+  }
+}
+
+export const contrast = {
+  title: 'contrast',
+
+  colors: {
+    primary: '#191919',
+    background: '#191919',
+    text: '#fff',
+    border: '#ff0400'
+  }
+}
