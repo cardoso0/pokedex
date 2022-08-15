fc198e9ea7c45bfcb0de9cb0281b3e224635df0b
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-15 17:46:31 -0300

    chore(PD): getting click function from context

--- src/components/MenuAccessibility/index.jsx
+++ src/components/MenuAccessibility/index.jsx
@@ -1,10 +1,12 @@
+import { useThemeContext } from "../../contexts/CustomTheme"
 
+export const MenuAccessibility = () => {
 
-export const MenuAccessibility = (props) => {
+  const { switchTheme } = useThemeContext()
 
   return (
     <div>
-      <button onClick={props.switch}>Altere o Tema</button>
+      <button onClick={switchTheme}>Altere o Tema</button>
     </div>
   )
 }
