e628b5be90a1a6fd791af923bc8d09b23058968e
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-28 12:45:22 -0300

    style(PD): cleaning code

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Bg = styled.footer`
-  // background-color: #ff0400;
   background: ${(props) => props.theme.colors.primary};
   text-align: center;
   padding: 1em;
