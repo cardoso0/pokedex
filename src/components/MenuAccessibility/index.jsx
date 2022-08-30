5e1ec516b59558cc8817e82556ad220fcacd5741
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-29 23:56:21 -0300

    chore(PD): cleaning code and added normaltheme funct

--- src/components/MenuAccessibility/index.jsx
+++ src/components/MenuAccessibility/index.jsx
@@ -1,21 +1,17 @@
 import { useThemeContext } from "../../hooks/useThemeContext"
 import { useTextSize } from "../../hooks/useTextSize"
-import { normal } from '../../style/themes/textSize/theme'
-import { useContext } from "react"
-import { ContextTextSize } from "../../contexts/ContextTextSize"
 
 export const MenuAccessibility = () => {
 
   const { switchTheme } = useThemeContext()
-  const { setTextTheme } = useContext(ContextTextSize);
-  const { textSizeIncrease, textSizeDecrease } = useTextSize()
+  const { textSizeIncrease, textSizeDecrease, normalTheme } = useTextSize()
 
   return (
     <div>
       <button onClick={switchTheme}>Altere o Tema</button>
       <div>
         <button onClick={textSizeDecrease}>A-</button>
-        <button onClick={() => setTextTheme(normal)}>A</button>
+        <button onClick={normalTheme}>A</button>
         <button onClick={textSizeIncrease}>A+</button>
       </div>
     </div>
