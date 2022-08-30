cfee27a326fe25ae301a0ab0e604a37fdd7b5b4a
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-29 22:40:47 -0300

    feat(PD): added buttons to change font size

--- src/components/MenuAccessibility/index.jsx
+++ src/components/MenuAccessibility/index.jsx
@@ -1,12 +1,23 @@
-import { useThemeContext } from "../../contexts/CustomTheme"
+import { useThemeContext } from "../../hooks/useThemeContext"
+import { useTextSize } from "../../hooks/useTextSize"
+import { normal } from '../../style/themes/textSize/theme'
+import { useContext } from "react"
+import { ContextTextSize } from "../../contexts/ContextTextSize"
 
 export const MenuAccessibility = () => {
 
   const { switchTheme } = useThemeContext()
+  const { setTextTheme } = useContext(ContextTextSize);
+  const { textSizeIncrease, textSizeDecrease } = useTextSize()
 
   return (
     <div>
       <button onClick={switchTheme}>Altere o Tema</button>
+      <div>
+        <button onClick={textSizeDecrease}>A-</button>
+        <button onClick={() => setTextTheme(normal)}>A</button>
+        <button onClick={textSizeIncrease}>A+</button>
+      </div>
     </div>
   )
 }
