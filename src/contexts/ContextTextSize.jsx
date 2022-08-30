324a058df88b1185fe98f1361ff75afe7a1e6bee
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-29 23:49:26 -0300

    refactor(PD): cleaning code and separated verification in localstorage

--- src/contexts/ContextTextSize.jsx
+++ src/contexts/ContextTextSize.jsx
@@ -1,13 +1,10 @@
 import { createContext, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { normal, size1 } from '../style/themes/textSize/theme'
+import { normal } from '../style/themes/textSize/theme'
 
 export const ContextTextSize = createContext()
 
 export const ContextTextSizeProvider = ({ children }) => {
-  
-  const theme = JSON.parse(localStorage.getItem('textSize'))
-  const verifyTheme = theme === null ? normal : theme
 
   const [textTheme, setTextTheme] = useState(normal)
 
