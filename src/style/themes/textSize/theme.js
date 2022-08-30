c9ebba8195d3c255e02899eae255c99ef70526ca
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-29 21:15:13 -0300

    feat(PD): new theme textsize

---
+++ src/style/themes/textSize/theme.js
@@ -0,0 +1,30 @@
+export const normal = {
+  title: 'normal',
+
+  text: {
+    textSize: '0rem',
+  }
+}
+export const size1 = {
+  text: {
+    textSize: '0.2rem',
+  }
+}
+
+export const size2 = {
+  text: {
+    textSize: '0.4rem',
+  }
+}
+
+export const size3 = {
+  text: {
+    textSize: '0.6rem',
+  }
+}
+
+export const size4 = {
+  text: {
+    textSize: '0.8rem',
+  }
+}
