939f00a306f28bbe6956c5323367e8169a21c521
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-29 23:47:47 -0300

    feat(PD): adjusted to get theme from localstorage

--- src/hooks/useTextSize.jsx
+++ src/hooks/useTextSize.jsx
@@ -4,36 +4,48 @@ import { normal, size1, size2, size3, size4 } from '../style/themes/textSize/the
 
 export const useTextSize = () => {
 
-  const { textTheme, setTextTheme } = useContext(ContextTextSize);
-  const [count, setCount] = useState(0)
+  const { textTheme, setTextTheme } = useContext(ContextTextSize)
+
+  const textSize = JSON.parse(localStorage.getItem('textSize'))
+  const verifyCount = textSize === null ? 0 : textSize
+  
+  const [count, setCount] = useState(verifyCount)
 
   const applyTheme = () => {
-    if (count === 0) setTextTheme(normal)
-    if (count === 1) setTextTheme(size1)
-    if (count === 2) setTextTheme(size2)
-    if (count === 3) setTextTheme(size3)
-    if (count === 4) setTextTheme(size4)
+    if (count === 0)  setTextTheme(normal)
+    if (count === 1)  setTextTheme(size1)
+    if (count === 2)  setTextTheme(size2)
+    if (count === 3)  setTextTheme(size3)
+    if (count === 4)  setTextTheme(size4)
   }
 
   const textSizeIncrease = () => {
     if (count < 4) {
       setCount(count + 1)
+      localStorage.setItem('textSize', JSON.stringify(count + 1))
     }
   }
-  
+
   const textSizeDecrease = () => {
     if (count >= 1) {
       setCount(count - 1)
+      localStorage.setItem('textSize', JSON.stringify(count - 1))
     }
   }
-  
+
+  const normalTheme = () => {
+    setTextTheme(normal)
+    localStorage.setItem('textSize', JSON.stringify(0))
+  }
+
   useEffect(() => {
     applyTheme()
-  },[count])
+  }, [count])
 
   return {
     textSizeIncrease,
     textSizeDecrease,
+    normalTheme,
     textTheme
   }
 }
