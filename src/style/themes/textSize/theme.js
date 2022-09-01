08b43bd658152eb1b413db950d7e6b0c34e8a135
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-08-31 22:46:47 -0300

    chore(PD): shortened object

--- src/style/themes/textSize/theme.js
+++ src/style/themes/textSize/theme.js
@@ -1,30 +1,5 @@
-export const normal = {
-  title: 'normal',
-
-  text: {
-    textSize: '0rem',
-  }
-}
-export const size1 = {
-  text: {
-    textSize: '0.2rem',
-  }
-}
-
-export const size2 = {
-  text: {
-    textSize: '0.4rem',
-  }
-}
-
-export const size3 = {
-  text: {
-    textSize: '0.6rem',
-  }
-}
-
-export const size4 = {
-  text: {
-    textSize: '0.8rem',
-  }
-}
+export const normal = { textSize: '0rem' }
+export const size1 = { textSize: '0.2rem' }
+export const size2 = { textSize: '0.4rem' }
+export const size3 = { textSize: '0.6rem' }
+export const size4 = { textSize: '0.8rem' }
