3aa107a5d8273847830078fb793f1d3b5004bd2f
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-09-29 17:51:02 -0300

    feat(PD): adding language for linux

---
+++ src/i18n/languages/en.js
@@ -0,0 +1,27 @@
+const messages = {
+  'en': {
+    translations: {
+      Title: {
+        title: "Are you ready for this journey?",
+        subtitle: "Form your Pokemon team!",
+        titleH: "Are you ready to start the journey?",
+        subtitleH: "Choose a Trainer"
+      },
+      Menu: {
+        fav: "favorites",
+      },
+      Form: {
+        placeholder: "Type a Pokemon",
+      },
+      Footer: {
+        copy: "Developed by Felipe Cardoso"
+      },
+      Home: {
+        title: "Don't falter like Ash",
+        btn: "Form your Pokémon team!"
+      }
+    }
+  }
+}
+
+export { messages }
