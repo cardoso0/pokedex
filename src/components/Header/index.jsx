c7efc0ad1580213ffa4cedc9f47f1ec6a77ebf3c
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-10-06 12:40:40 -0300

    feat(PD): added opt showmenu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,15 @@
 import * as S from "./style"
 import { Link } from 'react-router-dom'
 import pokedex from "../../assets/pokedex.png"
-import pokebola from "../../assets/favicon.png"
+import accessibility from "../../assets/accessibility/acess.png"
 import { Menu } from "../Menu"
 import { MenuAccessibility } from "../MenuAccessibility"
+import { ContextAccessibility } from "../../contexts/ContextAccessibility"
+import { useContext } from "react"
 
 export const Header = () => {
+
+  const { showMenu, setShowMenu } = useContext(ContextAccessibility)
 
   return (
     <S.Bg>
@@ -14,11 +18,11 @@ export const Header = () => {
           <img className="logo" src={pokedex} alt="logo pokedex" />
         </Link>
         <Menu />
-        <MenuAccessibility />
-        <Link to='/favorites'>
-          <img className="pokebola" src={pokebola} alt="pokebola" />
-        </Link>
+        <div onClick={() => !showMenu ? setShowMenu(true) : setShowMenu(false)}>
+          <img className="accessibility" src={accessibility} alt="accessibility" />
+        </div>
       </S.Header>
+        <MenuAccessibility teste={showMenu} />
     </S.Bg>
   )
 }
